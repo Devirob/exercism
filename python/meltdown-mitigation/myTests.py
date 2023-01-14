89a8bbb04d12b8b52efad37c9391baa482a9d08a
Devirob <robiszdev@gmail.com>
2023-01-15 00:38:25 +0100

    5/5 from python track

---
+++ python/meltdown-mitigation/myTests.py
@@ -0,0 +1,11 @@
+import conditionals
+
+print(conditionals.is_criticality_balanced(799.99,500.01))
+print(conditionals.is_criticality_balanced(750,650))
+
+print(conditionals.reactor_efficiency(10,400,10000))
+
+print(conditionals.fail_safe(10,1101,10000))
+
+print(conditionals.reactor_efficiency(10,999,10000))
+print(conditionals.reactor_efficiency(10,800,10000))
