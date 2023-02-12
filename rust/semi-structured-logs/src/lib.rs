86c660ba17a8cb8a86ce0c396e5690fbc26e9a01
Devirob <robiszdev@gmail.com>
2023-02-12 16:53:12 +0100

    Nice(?) solution from community

--- rust/semi-structured-logs/src/lib.rs
+++ rust/semi-structured-logs/src/lib.rs
@@ -2,6 +2,8 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+use std::fmt;
+
 /// various log levels
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum LogLevel {
@@ -11,32 +13,31 @@ pub enum LogLevel {
     Debug,
 }
 /// primary function for emitting logs
-pub fn log(level: LogLevel, message: &str) -> String {
-    match level {
-        LogLevel::Info => info(message),
-        LogLevel::Warning => warn(message),
-        LogLevel::Error => error(message),
-        LogLevel::Debug => debug(message),
+impl fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Info => write!(f, "INFO"),
+            Self::Warning => write!(f, "WARNING"),
+            Self::Error => write!(f, "ERROR"),
+            Self::Debug => write!(f, "DEBUG"),
+        }
     }
 }
 
-fn debug(message: &str) -> String {
-    let mut debug_label: String = "[DEBUG]: ".to_owned();
-    debug_label.push_str(message);
-    return debug_label;
+pub fn log(level: LogLevel, message: &str) -> String {
+    format!("[{level}]: {message}")
 }
+
+fn debug(message: &str) -> String {
+    log(LogLevel::Debug, message)
+}
+
 pub fn info(message: &str) -> String {
-    let mut info_label: String = "[INFO]: ".to_owned();
-    info_label.push_str(message);
-    return info_label;
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    let mut warn_label: String = "[WARNING]: ".to_owned();
-    warn_label.push_str(message);
-    return warn_label;
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    let mut error_label: String = "[ERROR]: ".to_owned();
-    error_label.push_str(message);
-    return error_label;
+    log(LogLevel::Error, message)
 }
